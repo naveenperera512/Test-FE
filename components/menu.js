8ea10e97602239066cdf79b37830e32c4f778105
naveen10qbit <naveen@10qbit.com>
2021-09-04 18:11:18 +0530

    category

--- components/menu.js
+++ components/menu.js
@@ -1,17 +1,36 @@
 export const menuItems = [
     {
         id: 1,
-        label: "menuitems.navigation.text",
-        isTitle: true
+        label: "Categories",
+        icon: "ri-book-2-line",
+        link: '/admin'
     },
     {
         id: 2,
+        label: "Job type",
+        icon: "ri-message-2-line",
+        link: '/sessions'
+    },
+    {
+        id: 3,
+        label: "Vacancy list",
+        icon: "ri-settings-2-line",
+        link: '/account_settings'
+    },
+    {
+        id: 4,
+        label: "User management",
+        icon: "ri-book-2-line",
+        link: '/education_content'
+    },
+    {
+        id: 5,
         label: "Education Content",
         icon: "ri-book-2-line",
         link: '/education_content'
     },
     {
-        id: 3,
+        id: 6,
         label: "menuitems.sessions.text",
         icon: "ri-message-2-line",
         link: '/sessions'
@@ -21,6 +40,6 @@ export const menuItems = [
         label: "menuitems.account_settings.text",
         icon: "ri-settings-2-line",
         link: '/account_settings'
-    }   
+    }
 ];
 
