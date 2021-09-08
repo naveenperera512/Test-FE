5e4c26fa4c392f4db8c9bdff2065e1f2fca4ddd9
Nirash10qbit <nirash@10qbit.com>
2021-09-08 17:30:46 +0530

    edit add vacancy

--- pages/employeer/myaccount/add-vacancy/test.vue
+++ pages/employeer/myaccount/add-vacancy/test.vue
@@ -26,9 +26,9 @@
               <b-form-input
                 id="title"
                 v-model="form.title"
+                type="text"
                 :state="errors && errors.title ? false : null"
                 placeholder="Some text value..."
-                type="text"
               />
               <b-form-invalid-feedback
                 v-if="errors && errors.title"
@@ -39,61 +39,107 @@
             </b-form-group>
 
             <div class="row">
-              <b-form-group class="col-md-12" label="Select Job Category" label-for="inputState">
-                <select class="form-control ">
-                  <option>
-                    Please select an option
-                  </option>
-                  <option v-for="(category, index) in categories.data" :key="index">
+              <!-- category-->
+              <b-form-group class="col-md-12" label-for="category_id">
+                <template #label>
+                  Category
+                  <span class="text-danger">*</span>
+                </template>
+                <b-form-select
+                  id="category_id"
+                  v-model="form.category_id"
+                  class="form-control "
+                  type="text"
+                  :state="errors && errors.category_id? false : null">
+                  <option value="">-- Please select an option --</option>
+                  <option v-for="category in categories.data" :value="category.id">
                     {{ category.name }}
                   </option>
-                </select>
+                </b-form-select>
+                <b-form-invalid-feedback
+                  v-if="errors && errors.category_id"
+                  id="telephone-feedback"
+                >
+                  {{ errors.category_id[0] }}
+                </b-form-invalid-feedback>
               </b-form-group>
-              <b-form-group class="col-md-12" label="Select Job Type" label-for="inputState">
-                <select class="form-control">
-                  <option>
-                    Please select an option
+
+              <!-- jobtype-->
+              <b-form-group class="col-md-12" label-for="job_type_id">
+                <template #label>
+                  Job Type
+                  <span class="text-danger">*</span>
+                </template>
+                <b-form-select
+                  id="job_type_id"
+                  v-model="form.job_type_id"
+                  class="form-control "
+                  type="text"
+                  :state="errors && errors.job_type_id? false : null">
+                  <option value="">-- Please select an option --</option>
+                  <option v-for="job_type in jobtypes.data" :value="job_type.id">
+                    {{ job_type.name }}
                   </option>
-                  <option v-for="(jobtype, index) in jobtypes.data" :key="index">
-                    {{ jobtype.name }}
-                  </option>
-                </select>
+                </b-form-select>
+                <b-form-invalid-feedback
+                  v-if="errors && errors.job_type_id"
+                  id="telephone-feedback"
+                >
+                  {{ errors.job_type_id[0] }}
+                </b-form-invalid-feedback>
               </b-form-group>
-              <b-form-group class="col-md-6" label="Select District" label-for="inputState">
-                <select class="form-control">
-                  <option>
-                    Please select an option
-                  </option>
-                  <option v-for="(district, index) in districts.data" :key="index">
+
+              <!-- district-->
+              <b-form-group class="col-md-6" label-for="district_id">
+                <template #label>
+                  District
+                  <span class="text-danger">*</span>
+                </template>
+                <b-form-select
+                  id="district_id"
+                  v-model="form.district_id"
+                  class="form-control "
+                  type="text"
+                  :state="errors && errors.district_id? false : null">
+                  <option value="">-- Please select an option --</option>
+                  <option v-for="district in districts.data" :value="district.id">
                     {{ district.name }}
                   </option>
-                </select>
+                </b-form-select>
+                <b-form-invalid-feedback
+                  v-if="errors && errors.district_id"
+                  id="telephone-feedback"
+                >
+                  {{ errors.district_id[0] }}
+                </b-form-invalid-feedback>
               </b-form-group>
-              <b-form-group class="col-md-6" label="Select City" label-for="inputState">
-                <select class="form-control">
-                  <option>
-                    Please select an option
-                  </option>
-                  <option v-for="(city, index) in cities.data" :key="index">
-                    {{ city.name }}
-                  </option>
-                </select>
-              </b-form-group>
-            </div>
-            <h6 class="sub-header">
-              Date of expiry
-            </h6>
 
-            <!-- Date-->
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label label="Date" label-cols-lg="2" label-cols-sm="2" label-for="date">Date <span
-                    class="text-danger">*</span></label>
-                  <b-form-input id="date" type="date" value="2019-08-19"/>
-                </div>
-              </div>
+              <!-- district city-->
+              <b-form-group class="col-md-6" label-for="district_city_id">
+                <template #label>
+                  District City
+                  <span class="text-danger">*</span>
+                </template>
+                <b-form-select
+                  id="district_city_id"
+                  v-model="form.district_city_id"
+                  class="form-control "
+                  type="text"
+                  :state="errors && errors.district_city_id? false : null">
+                  <option value="">-- Please select an option --</option>
+                  <option v-for="district_city in cities.data" :value="district_city.id">
+                    {{ district_city.name }}
+                  </option>
+                </b-form-select>
+                <b-form-invalid-feedback
+                  v-if="errors && errors.district_city_id"
+                  id="telephone-feedback"
+                >
+                  {{ errors.district_city_id[0] }}
+                </b-form-invalid-feedback>
+              </b-form-group>
             </div>
+
 
 
             <!-- description -->
@@ -102,37 +148,37 @@
                 <div class="form-group mt-3">
                   <label>Description</label>
                   <p class="text-muted font-13 m-b-30">Snow is a clean, flat toolbar theme.</p>
-                  <quill-editor :options="editorOption" style="min-height: 250px;"/>
+                  <quill-editor :options="editorOption" id="description" v-model="form.description" type="text" style="min-height: 250px;"/>
                 </div>
               </div>
             </div>
 
             <!-- file upload -->
-            <div class="row">
+            <!-- <div class="row">
               <div class="col-md-12">
                 <div class="form-group mt-3">
                   <label>Image Upload</label>
                   <p class="text-muted font-13 m-b-30">DropzoneJS is an open source library that provides drag’n’drop
                     file
                     uploads with image previews.</p>
-                  <vue-dropzone id="dropzone" ref="myVueDropzone" :options="dropzoneOptions" :use-custom-slot="true">
+                  <vue-dropzone id="cv_image" ref="myVueDropzone" :options="dropzoneOptions" v-model="form.cv_image" :use-custom-slot="true">
                     <div class="dz-message needsclick">
                       <i class="h1 text-muted ri-upload-cloud-2-line"></i>
                       <h3>Drop files here or click to upload.</h3>
-                      <span class="text-muted font-13">(This is just a demo dropzone. Selected files are
+                      <span class="text-muted font-13">(This is just a demo cv_image. Selected files are
                                 <strong>not</strong> actually uploaded.)</span>
                     </div>
                   </vue-dropzone>
 
                 </div>
               </div>
-            </div>
+            </div> -->
+
             <h6 class="sub-header">
               Employee details
             </h6>
 
             <div class="row">
-
               <!-- employee name-->
               <b-form-group class="col-md-6" label-for="input-employee_name">
                 <template #label>
@@ -142,9 +188,9 @@
                 <b-form-input
                   id="input-employee_name"
                   v-model="form.employee_name"
+                  type="text"
                   :state="errors && errors.employee_name ? false : null"
                   placeholder="Enter Name"
-                  type="text"
                 />
                 <b-form-invalid-feedback
                   v-if="errors && errors.employee_name"
@@ -153,16 +199,9 @@
                   {{ errors.employee_name[0] }}
                 </b-form-invalid-feedback>
               </b-form-group>
-            </div>
-
-            <h6 class="sub-header">
-              Contact details
-            </h6>
-
-            <div class="row">
 
               <!-- email address-->
-              <b-form-group class="col-md-6" label-for="employee-email">
+              <b-form-group class="col-md-6" label-for="employee_email">
                 <template #label>
                   Email Address
                   <span class="text-danger">*</span>
@@ -170,9 +209,9 @@
                 <b-form-input
                   id="employee-email"
                   v-model="form.employee_email"
+                  type="email"
                   :state="errors && errors.employee_email ? false : null"
                   placeholder="Enter your email"
-                  type="email"
                 />
                 <b-form-invalid-feedback
                   v-if="errors && errors.employee_email"
@@ -191,9 +230,9 @@
                 <b-form-input
                   id="telephone"
                   v-model="form.telephone"
+                  type="number"
                   :state="errors && errors.telephone ? false : null"
                   placeholder="(xx) xxx xxxx xxx"
-                  type="number"
                 />
                 <b-form-invalid-feedback
                   v-if="errors && errors.telephone"
@@ -221,36 +260,36 @@
 </template>
 
 <script>
-import vue2Dropzone from 'vue2-dropzone'
+// import vue2Dropzone from 'vue2-dropzone'
 
 /**
  * Checkout component
  */
 export default {
   components: {
-    vueDropzone: vue2Dropzone
+    // vueDropzone: vue2Dropzone
   },
   data() {
     return {
+      errors: {},
       form: {
-        // title: '',
-        // employee_name: '',
-        // employee_email: '',
-        telephone: '0710000001'
+        title: '',
+        user_id: '3',
+        slug: 'a',
+        category_id: '',
+        job_type_id: '',
+        district_id: '',
+        district_city_id: '',
+        employee_name: '',
+        employee_email: '',
+        telephone: '',
+        description: '',
+        // cv_image: ''
       },
       categories: {},
       jobtypes: {},
       districts: {},
       cities: {},
-      dropzoneOptions: {
-        url: 'https://httpbin.org/post',
-        thumbnailWidth: 150,
-        maxFilesize: 0.5,
-        headers: {
-          'My-Awesome-Header': 'header value'
-        },
-        previewTemplate: this.template()
-      },
       editorOption: {
         // Some Quill options...
         theme: "snow",
@@ -298,6 +337,15 @@ export default {
           ],
         },
       }
+      // dropzoneOptions: {
+      //   url: 'https://httpbin.org/post',
+      //   thumbnailWidth: 150,
+      //   maxFilesize: 0.5,
+      //   headers: {
+      //     'My-Awesome-Header': 'header value'
+      //   },
+      //   previewTemplate: this.template()
+      // }
     }
   },
 
@@ -308,11 +356,13 @@ export default {
     this.getCityList()
   },
 
+
   methods: {
-    async vacancies() {
+    async vacancies () {
       try {
+        await this.$axios.get('sanctum/csrf-cookie')
         await this.$axios.post('api/employee/vacancies', this.form)
-        await this.$router.replace({path: '/employeer/myaccount'})
+        await this.$router.replace({ path: '/employeer/myaccount' })
       } catch (error) {
         if (error.response.status === 422) {
           this.errors = error.response.data.errors
@@ -355,7 +405,7 @@ export default {
     },
     getCityList() {
       try {
-        this.$axios.get('api/cities')
+        this.$axios.get('api/districts/1/cities')
           .then((response) => {
             this.cities = (response.data)
           })
@@ -363,33 +413,34 @@ export default {
         // eslint-disable-next-line no-console
         console.log(error)
       }
-    },
+    }
 
-    template: function () {
-      return ` <div class="dropzone-previews mt-3">
-            <div class="card mt-1 mb-0 shadow-none border">
-                <div class="p-2">
-                    <div class="row align-items-center">
-                        <div class="col-auto">
-                            <img data-dz-thumbnail src="#" class="avatar-sm rounded bg-light" alt="">
-                        </div>
-                        <div class="col pl-0">
-                            <a href="javascript:void(0);" class="text-muted font-weight-bold" data-dz-name></a>
-                            <p class="mb-0" data-dz-size></p>
-                        </div>
-                        <div class="col-auto">
-                            <!-- Button -->
-      <a href="" class="btn btn-link btn-lg text-muted" data-dz-remove>
-      <i class="fe-x"></i>
-      </a>
-      </div>
-      </div>
-      </div>
-      </div>
-      </div>
-      `
-      },
+    // template: function () {
+    //   return ` <div class="dropzone-previews mt-3">
+    //         <div class="card mt-1 mb-0 shadow-none border">
+    //             <div class="p-2">
+    //                 <div class="row align-items-center">
+    //                     <div class="col-auto">
+    //                         <img data-dz-thumbnail src="#" class="avatar-sm rounded bg-light" alt="">
+    //                     </div>
+    //                     <div class="col pl-0">
+    //                         <a href="javascript:void(0);" class="text-muted font-weight-bold" data-dz-name></a>
+    //                         <p class="mb-0" data-dz-size></p>
+    //                     </div>
+    //                     <div class="col-auto">
+    //                         <!-- Button -->
+    //   <a href="" class="btn btn-link btn-lg text-muted" data-dz-remove>
+    //   <i class="fe-x"></i>
+    //   </a>
+    //   </div>
+    //   </div>
+    //   </div>
+    //   </div>
+    //   </div>
+    //   `
+    //   }
   }
 }
 
 </script>
+
